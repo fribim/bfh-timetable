8afab7164abaca751acd2b5cdf5eaf16155b49b8
Fribim <jan.friedli@gmx.ch>
2017-09-20 14:34:17 +0200

    display the timetable a bit better looking ;)

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RestService } from '../../service/rest.service';
 import * as _ from "lodash";
+import { LoadingController, ToastController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
@@ -21,17 +23,37 @@ export class HomePage {
   ];
 
   constructor(
-      private service: RestService
+      private service: RestService,
+      private loadingCtrl: LoadingController,
+      private toastCtrl: ToastController
   ) {
 
   }
 
   ionViewDidLoad () {
+
+    let loader = this.loadingCtrl.create({
+      content: "Lade Stundenplan",
+      spinner: "crescent"
+    });
+
+    loader.present();
+
     this.service.getTimeTable().then((data) => {
+      loader.dismiss();
       this.courses = _.filter(data.json().data, { classname: 'I1a' });
     }).catch((ex) => {
-      console.error('failed loading');
+      loader.dismiss();
+      this.couldNotLoadData();
     });
   }
 
+  private couldNotLoadData() {
+    var toast = this.toastCtrl.create({
+      message: 'Stundenplan konnte nicht geladen werden',
+      duration: 5000,
+      position: "top"
+    });
+    toast.present();
+  }
 }
